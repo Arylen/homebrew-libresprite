13e47a88d27df05d38ae657aad0d6b146c58a7d3
Arylen <177881981+Arylen@users.noreply.github.com>
2026-03-11 09:02:31 -0500

    Initial commit.

---
+++ Formula/libresprite.rb
@@ -0,0 +1,78 @@
+class Libresprite < Formula
+  desc "Free and open source pixel art and sprite animation editor"
+  homepage "https://libresprite.github.io/"
+  url "https://github.com/LibreSprite/LibreSprite.git",
+      tag:      "v1.1",
+      revision: "dce8cfe"
+  license "GPL-2.0-only"
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "freetype"
+  depends_on "giflib"
+  depends_on "gnutls"
+  depends_on "jpeg-turbo"
+  depends_on "libarchive"
+  depends_on "libpng"
+  depends_on "libwebp"
+  depends_on "pixman"
+  depends_on "sdl2"
+  depends_on "sdl2_image"
+  depends_on "tinyxml2"
+  depends_on "zlib"
+
+  on_macos do
+    depends_on xcode: :build
+  end
+
+  def install
+    # Ensure submodules are initialised (Homebrew clones with --depth=1)
+    system "git", "submodule", "update", "--init", "--recursive"
+
+    mkdir "build" do
+      args = std_cmake_args + %W[
+        -G Ninja
+        -DUSE_SHARED_TINYXML2=ON
+        -DUSE_SHARED_PIXMAN=ON
+        -DUSE_SHARED_FREETYPE=ON
+        -DUSE_SHARED_GIFLIB=ON
+        -DUSE_SHARED_JPEGLIB=ON
+        -DUSE_SHARED_ZLIB=ON
+        -DUSE_SHARED_LIBPNG=ON
+        -DUSE_SHARED_LIBWEBP=ON
+        -DUSE_SHARED_LIBARCHIVE=ON
+        -DCMAKE_PREFIX_PATH=#{Formula["libarchive"].opt_prefix}
+      ]
+
+      if OS.mac?
+        sdk = MacOS::CLT.installed? ? "" : MacOS.sdk_path.to_s
+        args << "-DCMAKE_OSX_SYSROOT=#{sdk}" unless sdk.empty?
+      end
+
+      system "cmake", "..", *args
+      system "ninja", "libresprite"
+
+      # Install the binary and data
+      bin.install "bin/libresprite"
+      prefix.install Dir["bin/data"] if File.directory?("bin/data")
+    end
+  end
+
+  def caveats
+    <<~EOS
+      LibreSprite has been installed as a command-line binary.
+      Run it with:
+        libresprite
+
+      If you want an app bundle in /Applications, you can create
+      a wrapper with Automator or a shell script .app bundle.
+    EOS
+  end
+
+  test do
+    # LibreSprite needs a display; just verify the binary exists and runs
+    assert_match "LibreSprite", shell_output("#{bin}/libresprite --version 2>&1", 1)
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# LibreSprite Homebrew Formula
+
+Just a quick formula for pulling, building, and installing LibreSprite on MacOS, since the releases are broken for Mac.
+
+## Installation
+
+1. Install [https://brew.sh/](Homebrew)
+2. Run `brew tap arylen/libresprite`
+3. Run `brew install libresprite`
